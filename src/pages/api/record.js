e624ce29fa7c2c857fca06197c37fcbf98886f8b
韩旭gogogo <js_bin@163.com>
2023-10-17 11:14:57 +0800

    feature: T_RECORDS logics add

--- src/pages/api/record.js
+++ src/pages/api/record.js
@@ -1,4 +1,8 @@
 // === Utils === //
+import sumBy from 'lodash/sumBy'
+import some from 'lodash/some'
+import isString from 'lodash/isString'
+import isNumber from 'lodash/isNumber'
 import getMysqlInstance from '@/utils/mysqlFactory'
 
 export async function record(req, res) {
@@ -7,15 +11,46 @@ export async function record(req, res) {
         // create the connection
         const connection = await getMysqlInstance();
         // query database
-        const [rows, fields] = await connection.execute('SELECT * FROM `T_USER`', []);
-        console.log('rows, fields=', rows, fields)
+        const [rows, fields] = await connection.execute('SELECT * FROM `T_RECORDS`', []);
         res.status(200).json(rows);
         return
     }
 
     if (req.method === 'POST') {
+        try {
+            valid(req)
+        } catch (error) {
+            console.error('error=', error)
+            res.status(400).json({ message: error.message });
+            return
+        }
+        const { body } = req
+        const connection = await getMysqlInstance();
+        const groupId = Date.now()
+        console.log('groupId=', groupId, body)
+        body.forEach(element => {
+            const { name, score } = element
+            connection.query('INSERT INTO T_RECORDS (name, score, group_id) VALUES(?,?,?)', [name, score, groupId], (error,
+                results) => {
+                if (error) return res.json({ error: error });
+
+            });
+        });
+
+        res.status(200).json({ message: 'ok' });
         return
     }
+}
+
+function valid(req) {
+    const { body } = req
+    if (!Array.isArray(body)) {
+        throw new Error('Array type!')
+    }
+    if (some(body, i => !isString(i.name)) || some(body, i => !isNumber(i.score)) || sumBy(body, i => i.score) !== 0) {
+        throw new Error('Invalid values!')
+    }
+    return body
 }
 
 export default record
